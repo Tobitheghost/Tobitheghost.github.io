c2dc8abf592ce5dbe6921cb7ea74624754b556fb
Tobitheghost <kklogos13@gmail.com>
2023-07-28 22:06:23 -0500

    adding contact sheet

--- website/index.py
+++ website/index.py
@@ -5,6 +5,11 @@ homePage = Blueprint('homePage', __name__)
 
 @homePage.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        contact_msg = request.form.get('contact_msg')
+        return f"{name} \n{email} \n{contact_msg}"
     return render_template("index.html")
 
 @homePage.route("/movies")
