8360a9b5c111d9218a10f357e2d72ad70d9fc31a
Tobitheghost <kklogos13@gmail.com>
2023-07-29 20:23:38 -0500

    adding contact info
    smtp email server setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
-from os import path
+from flask_mail import Mail
+from .utils import mailUsername, mailPassword
+
+app = Flask(__name__)
+
+mail = Mail(app)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = mailUsername
+app.config['MAIL_PASSWORD'] = mailPassword
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 def create_app():
-    app = Flask(__name__)
-
     from .index import homePage
     app.register_blueprint(homePage, url_prefix='/')
     return app
--- website/index.py
+++ website/index.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-import os
+from flask_mail import Mail, Message
+from .utils import mailUsername
+from website import mail
+
 
 homePage = Blueprint('homePage', __name__)
 
@@ -9,7 +12,11 @@ def home():
         name = request.form.get('name')
         email = request.form.get('email')
         contact_msg = request.form.get('contact_msg')
-        return f"{name} \n{email} \n{contact_msg}"
+        msg = Message(subject=f"ContactWebpage: {name} at {email}", body=contact_msg, recipients=[mailUsername], sender=email)
+        mail.send(msg)
+        msg2 = Message(subject=f"Thank You for Contacting Me!", body="Thank you for getting in contact with me. I will read your message asap!", recipients=[email], sender=mailUsername)
+        mail.send(msg2)
+        return render_template("index.html", success=True)
     return render_template("index.html")
 
 @homePage.route("/movies")
